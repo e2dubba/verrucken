7e6ff9faa7244740bbc69b81a363a26d37b692ca
shawngoodwin <svg@shulgi.attlocal.net>
2016-12-18 22:14:11 -0500

    fix indenting

--- verrucken.js
+++ verrucken.js
@@ -1,15 +1,16 @@
 // Vimperator Plugin: 'gtr'
 // Usuage: use :gtr
 
-commands.addUserCommand(['gtr'], 'Opens Clipboard text in Google Translate', 
+commands.addUserCommand(['gtr'], 'Opens Clipboard text in Google Translate',
 	function () {
         let otext = util.readFromClipboard()
         otext = otext.replace(/\-\r?\n/g, '');
         otext = otext.replace(/\r?\n/g, ' ');
-		var url = "https://translate.google.com/#auto/en/" ;
-	    url += encodeURIComponent(otext);
+				var url = "https://translate.google.com/#auto/en/" ;
+	    	url += encodeURIComponent(otext);
         liberator.open(url,
-            liberator[options.get("activate").has("paste") ? "NEW_TAB" : "NEW_BACKGROUND_TAB"]);
+            liberator[options.get("activate").has("paste")
+							? "NEW_TAB" : "NEW_BACKGROUND_TAB"]);
 
     }
 );
