691bd44245d66d8357a33bdda23f27436b9ce3b3
e2dubba <svirgilgoodwin@gmail.com>
2017-01-08 20:47:01 -0600

    creating scripts and more

--- verrucken.js
+++ verrucken.js
@@ -1,16 +1,15 @@
 // Vimperator Plugin: 'gtr'
 // Usuage: use :gtr
 
-commands.addUserCommand(['gtr'], 'Opens Clipboard text in Google Translate',
-	function () {
-        let otext = util.readFromClipboard()
+document.addEvenListener('DOMContentLoaded',  function() {
+    var checkPageButton = document.getElementById('getTranslation');
+    checkPageButton.addEventListener('click', function(){
+
+        var otext = document.execCommand('paste');
         otext = otext.replace(/\-\r?\n/g, '');
         otext = otext.replace(/\r?\n/g, ' ');
-				var url = "https://translate.google.com/#auto/en/" ;
-	    	url += encodeURIComponent(otext);
-        liberator.open(url,
-            liberator[options.get("activate").has("paste")
-							? "NEW_TAB" : "NEW_BACKGROUND_TAB"]);
-
-    }
-);
+		var url = "https://translate.google.com/#auto/en/" ;
+	    url += encodeURIComponent(otext);
+        chrome.tabs.create(url);
+    });
+    }, false);
